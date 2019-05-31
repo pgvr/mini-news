3c884241b90487e2a72248a1b79580e08cb1756f
Patrick Göler von Ravensburg <patrickvongoeler@gmail.com>
2019-05-31 21:06:48 +0200

    change bootstrap import order, style home header

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  categories = ['All', 'Technology', 'Politics', 'Sports'];
 
-  constructor() { }
+  selectedCategory: string;
+
+  constructor() {}
 
   ngOnInit() {
+    this.selectCategory('All');
   }
 
+  selectCategory(category: string) {
+    this.selectedCategory = category;
+  }
 }
