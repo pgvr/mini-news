9ed399f0e72537003f52f1a5aa89dbfa4077a564
Patrick Göler von Ravensburg <patrickvongoeler@gmail.com>
2019-05-31 22:54:56 +0200

    news item liste

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from 'src/app/services/news.service';
+import { NewsItem } from 'src/app/models/news-item';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +12,21 @@ export class HomeComponent implements OnInit {
 
   selectedCategory: string;
 
-  constructor() {}
+  loading: boolean;
+
+  newsItems: NewsItem[];
+
+  constructor(private newsService: NewsService) {}
 
   ngOnInit() {
     this.selectCategory('All');
+    this.getNews();
+  }
+
+  async getNews() {
+    this.loading = true;
+    this.newsItems = await this.newsService.getNews();
+    this.loading = false;
   }
 
   selectCategory(category: string) {
--- src/app/components/news-card/news-card.component.ts
+++ src/app/components/news-card/news-card.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { NewsItem } from 'src/app/models/news-item';
+import {
+  DomSanitizer,
+  SafeResourceUrl,
+  SafeUrl,
+  SafeStyle,
+} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-news-card',
   templateUrl: './news-card.component.html',
-  styleUrls: ['./news-card.component.scss']
+  styleUrls: ['./news-card.component.scss'],
 })
 export class NewsCardComponent implements OnInit {
-
-  constructor() { }
+  @Input() newsItem: NewsItem;
+  @Output() cardClickEvent: EventEmitter<NewsItem> = new EventEmitter<
+    NewsItem
+  >();
+  backgroundUrl: SafeStyle;
+  constructor(private sanitizer: DomSanitizer) {}
 
   ngOnInit() {
+    this.backgroundUrl = this.sanitizer.bypassSecurityTrustStyle(
+      `linear-gradient(360deg, #000000 22.4%, rgba(0, 0, 0, 0) 100%), url(${this
+        .newsItem.thumbnail || this.newsItem.enclosure.link})`,
+    );
   }
 
+  imagePresent(): boolean {
+    const thumbnailPresent: boolean =
+      this.newsItem.thumbnail !== undefined && this.newsItem.thumbnail !== '';
+    const enclosurePresent: boolean =
+      this.newsItem.enclosure.link !== undefined &&
+      this.newsItem.enclosure.link !== '';
+    return enclosurePresent || thumbnailPresent;
+  }
+
+  cardClick() {
+    this.cardClickEvent.emit(this.newsItem);
+  }
 }
