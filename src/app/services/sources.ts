9b5482b5c359d22495c2f94c6d18a4738dfffa0d
Patrick Göler von Ravensburg <patrickvongoeler@gmail.com>
2019-06-02 15:11:00 +0200

    unify rss urls

--- src/app/services/sources.ts
+++ src/app/services/sources.ts
@@ -1,42 +1,42 @@
 export const generalSources = [
-  'https://rss.dw.com/xml/rss-de-top/',
-  'https://www.tagesschau.de/xml/rss2/',
-  'https://www.n-tv.de/rss/',
-  'https://www.welt.de/feeds/topnews.rss/',
-  'https://newsfeed.zeit.de/all/',
-  'https://www.faz.net/rss/aktuell/',
+  'https://rss.dw.com/xml/rss-de-top',
+  'https://www.tagesschau.de/xml/rss2',
+  'https://www.n-tv.de/rss',
+  'https://www.welt.de/feeds/topnews.rss',
+  'https://newsfeed.zeit.de/all',
+  'https://www.faz.net/rss/aktuell',
 ];
 
 export const politicSources = [
-  'https://rss.dw.com/xml/rss_de_politik/',
-  'https://www.n-tv.de/politik/rss/',
-  'https://www.welt.de/feeds/section/politik.rss/',
-  'https://www.faz.net/rss/aktuell/politik/',
+  'https://rss.dw.com/xml/rss_de_politik',
+  'https://www.n-tv.de/politik/rss',
+  'https://www.welt.de/feeds/section/politik.rss',
+  'https://www.faz.net/rss/aktuell/politik',
 ];
 
 export const economySources = [
-  'https://rss.dw.com/xml/rss-de-eco/',
-  'https://www.n-tv.de/wirtschaft/rss/',
-  'https://www.welt.de/feeds/section/wirtschaft.rss/',
-  'https://www.faz.net/rss/aktuell/wirtschaft/',
+  'https://rss.dw.com/xml/rss-de-eco',
+  'https://www.n-tv.de/wirtschaft/rss',
+  'https://www.welt.de/feeds/section/wirtschaft.rss',
+  'https://www.faz.net/rss/aktuell/wirtschaft',
 ];
 
 export const sportSources = [
-  'https://rss.dw.com/xml/rss-de-sport/',
-  'https://www.sportschau.de/sportschauindex100_type-rss.feed/',
-  'https://www.welt.de/feeds/section/sport.rss/',
-  'https://www.faz.net/rss/aktuell/sport/',
+  'https://rss.dw.com/xml/rss-de-sport',
+  'https://www.sportschau.de/sportschauindex100_type-rss.feed',
+  'https://www.welt.de/feeds/section/sport.rss',
+  'https://www.faz.net/rss/aktuell/sport',
 ];
 
 export const scienceSources = [
-  'https://rss.dw.com/xml/rss-de-wissenschaft/',
-  'https://www.welt.de/feeds/section/wissenschaft.rss/',
-  'https://www.faz.net/rss/aktuell/wissen/',
+  'https://rss.dw.com/xml/rss-de-wissenschaft',
+  'https://www.welt.de/feeds/section/wissenschaft.rss',
+  'https://www.faz.net/rss/aktuell/wissen',
 ];
 
 export const techSources = [
-  'https://www.heise.de/newsticker/heise.rdf/',
-  'https://rss.golem.de/rss.php?feed=ATOM1.0/',
-  'https://www.faz.net/rss/aktuell/technik-motor/',
-  'https://www.welt.de/feeds/section/wirtschaft/webwelt.rss/',
+  'https://www.heise.de/newsticker/heise.rdf',
+  'https://rss.golem.de/rss.php?feed=ATOM1.0',
+  'https://www.faz.net/rss/aktuell/technik-motor',
+  'https://www.welt.de/feeds/section/wirtschaft/webwelt.rss',
 ];
