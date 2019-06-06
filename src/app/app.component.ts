e4b1246a1e42ab3a2bc50763232ff6f6263f7511
Patrick Göler von Ravensburg <patrickvongoeler@gmail.com>
2019-06-06 15:56:29 +0200

    add gtag analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { UpdateService } from './services/update.service';
 import { NewsService } from './services/news.service';
+// declare ga as a function to set and sent the events
+// tslint:disable-next-line: ban-types
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -9,6 +12,18 @@ import { NewsService } from './services/news.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(public update: UpdateService, public newsService: NewsService) {}
+  constructor(
+    public update: UpdateService,
+    public newsService: NewsService,
+    public router: Router,
+  ) {
+    // subscribe to router events and send page views to Google Analytics
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        gtag('set', 'page', event.urlAfterRedirects);
+        gtag('send', 'pageview');
+      }
+    });
+  }
   ngOnInit() {}
 }
